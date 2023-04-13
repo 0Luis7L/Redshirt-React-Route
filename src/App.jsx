b7460cbcff3d9bf03e02cdeed3207d13ed9c503e
0Luis7L <105124502+0Luis7L@users.noreply.github.com>
2023-04-13 12:56:27 -0700

    Added Login Component

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,13 @@ import RootLayout from './layouts/RootLayout'
 import data from './data'
 import LaptopDetails from './pages/LaptopDetails'
 import LaptopsList from './pages/LaptopList'
+import Login from '../../../Login/Login-app/src/Login'
 
 // Update //
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home img="../images/greenLogo.png" />}/>
+      <Route index element={<Login />}/>
       <Route path="upload/:idx" element={<LaptopDetails />} action={uploadAction}/>
       <Route path="/unlisted" element={<LaptopsList/>} /> 
     </Route>
