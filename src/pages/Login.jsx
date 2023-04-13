3acfa131cb22f4d1addef3d9cb08e318c7b0bd60
0Luis7L <105124502+0Luis7L@users.noreply.github.com>
2023-04-13 13:51:44 -0700

    update Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,21 @@
 
 
-
-function Login(){
-
-
+// All this should be new!!!
+export default function Login(){
 
     return ( 
-    {/*  PUT ALL THE LOGIN STUFF HERE PLS */}
+        <div className="login-card">
+        <form>
+            <div className='login-heading'>
+                <h1 className="login">Welcome</h1>
+                <h2 className="login">Login</h2>
+            </div>
+            <div className="login-inputs">
+                <input className="login--input" type="email" placeholder="email"/>
+                <input className="login--input" type="text" placeholder="password" />
+            </div>
+            <button className='loginBTN' type='submit'>Login</button>
+        </form>
+    </div>
     )
 }
