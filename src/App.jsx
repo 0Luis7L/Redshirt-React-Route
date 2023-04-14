6d2c2887852286639fb3647193ea514ef0a2f485
collin-living-green-tech <collin@livinggreentechnology.org>
2023-04-14 08:06:18 -0700

    Fixed the navbar navigation crash issue

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,21 @@ import {
 from 'react-router-dom'
 // Pages
 import LaptopDetails from './pages/LaptopDetails'
-import Upload, { uploadAction } from './pages/Upload'
+
 import LaptopsList from './pages/LaptopList'
-import Login from '../../../Login/Login-app/src/Login'
+import Login from './pages/Login'
 // Layouts
 import RootLayout from './layouts/RootLayout'
+import Home from './pages/Home'
 
 // Update //
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<Login />}/>
-      <Route path="upload/:idx" element={<LaptopDetails />} action={uploadAction}/>
-      <Route path="unlisted" element={<LaptopsList/>} /> 
+      <Route path="/" element={<Login />} />
+      <Route path="/unlisted" element={<LaptopsList/>} /> 
+      <Route path="/upload/:idx" element={<LaptopDetails />} />
+      
     </Route>
   )
 )
